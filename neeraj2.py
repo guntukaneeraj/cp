71754055cacbdab22f0e8ca5429b321e5e99d337
guntukaneeraj <163829737+guntukaneeraj@users.noreply.github.com>
2024-03-18 13:53:21 +0530

    Add files via upload

---
+++ neeraj2.py
@@ -0,0 +1,13 @@
+#Maximum Binary Tree
+class Solution:
+    def constructMaximumBinaryTree(self, nums:
+        if len(nums)==0:
+            return
+        else:
+            ma= max(nums)
+            index =nums.index(ma)
+            root =TreeNode(ma)
+            root.left=self.constructMaximumBinaryTree(nums[0:index])
+            root.right=self.constructMaximumBinaryTree(nums[index+1:])
+        return root
+    
